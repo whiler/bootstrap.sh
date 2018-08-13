c5e23ddb0607e18723eb6725e39ff091ea7180dd
whiler <whiler@users.noreply.github.com>
2018-08-13 10:50:51 +0800

    prompt-toolkit<2.0.0,>=1.0.15

--- update.sh
+++ update.sh
@@ -11,8 +11,14 @@ gitupdate() {
 
 brew upgrade
 
-(pip3 freeze | cut -d= -f1 > /tmp/r.txt && echo "wheel" >> /tmp/r.txt && echo "pip" >> /tmp/r.txt && echo "setuptools" >> /tmp/r.txt && pip3 install -U -r /tmp/r.txt && rm /tmp/r.txt)&
-
+(pip3 freeze | cut -d= -f1 > /tmp/r.txt && \
+ echo "wheel" >> /tmp/r.txt && \
+ echo "pip" >> /tmp/r.txt && \
+ echo "setuptools" >> /tmp/r.txt && \
+ sed -i '' -e 's/prompt-toolkit/prompt-toolkit<2.0.0,>=1.0.15/' /tmp/r.txt && \
+ pip3 install -U -r /tmp/r.txt && \
+ rm /tmp/r.txt)&
+ 
 if [[ -e "${HOME}/.oh-my-zsh" ]]; then
 	gitupdate "${HOME}/.oh-my-zsh" &
 fi
