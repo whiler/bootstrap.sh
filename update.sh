9d656550220a7031c483960837ec80f26203b584
whiler <wenwu500@qq.com>
2021-01-26 23:54:24 +0800

    pip3 ignore local packages

--- update.sh
+++ update.sh
@@ -15,13 +15,16 @@ brew cleanup -s
 brew doctor
 brew missing
 
-(pipdeptree | grep -o -E '^\w[^=]+' > /tmp/r.txt && \
- echo "wheel" >> /tmp/r.txt && \
- echo "pip" >> /tmp/r.txt && \
- echo "setuptools" >> /tmp/r.txt && \
- sed -i '' -e 's/prompt-toolkit/prompt-toolkit<2.1.0,>=2.0.0/' /tmp/r.txt && \
- pip3 install -U -r /tmp/r.txt && \
- rm /tmp/r.txt)&
+pip3 freeze | grep 'file://' | cut -d ' ' -f 1 | sort -u > /tmp/e.txt
+pipdeptree | grep -o -E '^\w[^=]+' > /tmp/r.txt && \
+	echo "wheel" >> /tmp/r.txt && \
+	echo "pip" >> /tmp/r.txt && \
+	echo "setuptools" >> /tmp/r.txt && \
+	sort -u < /tmp/r.txt > /tmp/rr.txt && \
+	sed -i '' -e 's/prompt-toolkit/prompt-toolkit<2.1.0,>=2.0.0/' /tmp/rr.txt
+comm -23 /tmp/rr.txt /tmp/e.txt > /tmp/pkgs.txt
+pip3 install -U -r /tmp/pkgs.txt
+rm /tmp/pkgs.txt /tmp/rr.txt /tmp/e.txt /tmp/r.txt
 
 if [[ -e "${HOME}/.oh-my-zsh" ]]; then
     gitupdate "${HOME}/.oh-my-zsh" &
