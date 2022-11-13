ab0a9bc1cee140ea08a27f6dbc6f51d7677699d1
whiler <wenwu500@qq.com>
2022-11-13 10:37:59 +0800

    forget upgrade pkgs with pipdeptree

--- update.sh
+++ update.sh
@@ -8,23 +8,13 @@ gitupdate() {
 	popd || return
 }
 
+brew leaves
 brew update
 brew upgrade
 brew cleanup -s
 brew autoremove
 brew doctor
 brew missing
-
-python3 -m pip freeze | grep 'file://' | cut -d ' ' -f 1 | sort -u >/tmp/e.txt
-python3 -m pipdeptree | grep -o -E '^\w[^=]+' >/tmp/r.txt &&
-	echo "wheel" >>/tmp/r.txt &&
-	echo "pip" >>/tmp/r.txt &&
-	echo "setuptools" >>/tmp/r.txt &&
-	sort -u </tmp/r.txt >/tmp/rr.txt &&
-	sed -i '' -e 's/prompt-toolkit/prompt-toolkit<2.1.0,>=2.0.0/' /tmp/rr.txt
-comm -23 /tmp/rr.txt /tmp/e.txt >/tmp/pkgs.txt
-python3 -m pip install -U -r /tmp/pkgs.txt
-rm /tmp/pkgs.txt /tmp/rr.txt /tmp/e.txt /tmp/r.txt
 
 if [[ -e "${HOME}/.oh-my-zsh" ]]; then
 	gitupdate "${HOME}/.oh-my-zsh" &
